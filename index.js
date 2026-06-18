81f0dadcfc6c2eb0ad8516d85e8c4cdc5e975aa0
Hamdy Osama <hamdyosama098@gmail.com@gmail.com>
2026-06-18 16:11:40 +0300

    Initial project setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const express=require("express")
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT=process.env.PORT || 5000;
+const app=express()
+
+
+
+app.listen(PORT,()=>{
+    console.log(`app is listen on port : ${PORT}`)
+})
+
+
